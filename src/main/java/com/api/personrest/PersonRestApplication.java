1c95951b53130af4f25a9c33a6e673ff74a1abb0
Cairo Pereira <cairo.151295@gmail.com>
2020-06-04 13:22:24 -0300

    Inicializar projeto.

---
+++ src/main/java/com/api/personrest/PersonRestApplication.java
@@ -0,0 +1,13 @@
+package com.api.personrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/personrest/PersonRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.personrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
